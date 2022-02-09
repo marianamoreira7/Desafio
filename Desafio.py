284551e1c572e604712afcdb39cec792edb94acd
Mariana Moreira <95227387+marianamoreira7@users.noreply.github.com>
2022-02-09 14:14:07 -0300

    Add files via upload
    Desafio Maior Numero em Python

---
+++ Desafio.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+num3 = int(input('Enter 2nd number: '))
+maxNumber = num1
+if(num2 > maxNumber):
+    maxNumber = num2
+if(num3 > maxNumber):
+    maxNumber = num3
+
+print('o Maior numero eh ',maxNumber)
