4d9cc4e0e0def4ee4d72f7669eca8e88d8d1f878
Mariana Moreira <95227387+marianamoreira7@users.noreply.github.com>
2022-02-09 14:14:57 -0300

    Update Desafio.py

--- Desafio.py
+++ Desafio.py
@@ -1,10 +1,10 @@
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
-num3 = int(input('Enter 2nd number: '))
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero '))
+num3 = int(input('Digite o terceiro numero '))
 maxNumber = num1
 if(num2 > maxNumber):
     maxNumber = num2
 if(num3 > maxNumber):
     maxNumber = num3
 
-print('o Maior numero eh ',maxNumber)
+print('o maior numero eh ',maxNumber)
